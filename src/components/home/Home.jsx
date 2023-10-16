f4c6498a01663c9ba72eb9e4bd2fce82e1047bf6
justMG <mgokceteoman@gmail.com>
2023-10-16 19:21:27 +0300

    betaV0.1

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArcRotateCamera, Vector3, HemisphericLight, SceneLoader  } from '@babylonjs/core'
+import { ArcRotateCamera, Vector3, HemisphericLight, SceneLoader } from '@babylonjs/core'
 import '@babylonjs/loaders'
 import Smple from '../smple/Smple';
 import './Home.css'
@@ -15,16 +15,17 @@ const Canvas = props => {
         //const scene = new Scene(engine);
         const canvas = scene.getEngine().getRenderingCanvas();
 
-        const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 0, 0));
+        const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 12, -6));
         camera.attachControl(canvas, true);
         const light = new HemisphericLight("light", new Vector3(1, 1, 0));
-        
-        await SceneLoader.ImportMeshAsync("", "/src/assets/box/", 'scene.babylon',
-        scene, function(meshes){
 
-            scene.createDefaultCameraOrLight(true,true,true);
-            scene.createDefaultEnvironment();
-        });
+        const globeMesh = await SceneLoader.ImportMeshAsync("", "src/assets/box/", 'scene.babylon',
+            scene, function (meshes) {
+            });
+        const anima = globeMesh.meshes[0]
+        anima.scaling = new Vector3(1, 1, 1)
+        anima.rotation = new Vector3(0, 3, 0)
+        console.log(globeMesh);
 
 
         // SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon");
@@ -50,6 +51,6 @@ const Canvas = props => {
         //return scene;
         //id ='base-canvas'
     };
-    return <Smple onSceneReady = {createScene}{...props} id ='base-canvas' />
+    return <Smple onSceneReady={createScene}{...props} id='base-canvas' />
 }
 export default Canvas
